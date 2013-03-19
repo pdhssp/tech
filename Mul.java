768bbe651695186e0e72a7b6318738cd3c924ff3
lahirumadushanka <lahirumadushanka2100@gmail.com>
2013-03-19 21:06:59 +0530

    first branch

---
+++ Mul.java
@@ -0,0 +1,10 @@
+public class Mul{
+	public static void main(String[] args){
+		for(int a=1;a<=12;a++){
+			for(int b=1;b<=12;b++){
+				System.out.print(b);
+			}	
+		System.out.println();
+		}
+	}
+}
